391f1464e881f34c248e60a31c586553419f0af9
Stefan Reimer <stefan@zero-downtime.net>
2020-12-29 00:14:06 +0100

    Set version

--- cloudbender/__init__.py
+++ cloudbender/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 __author__ = "Stefan Reimer"
 __email__ = "stefan@zero-downtimet.net"
-__version__ = "0.9.5"
+__version__ = "0.9.6"
 
 
 # Set up logging to ``/dev/null`` like a library is supposed to.
