e3cb29c45414abb1fd6812bbeaeb5b2d340adab5
Stefan Reimer <stefan@zero-downtime.net>
2022-02-08 14:14:24 +0100

    ci: Add Jenkins support, set Python version based on tags

--- cloudbender/__init__.py
+++ cloudbender/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 __author__ = "Stefan Reimer"
 __email__ = "stefan@zero-downtimet.net"
-__version__ = "0.10.2"
+__version__ = "v0.10.3"
 
 
 # Set up logging to ``/dev/null`` like a library is supposed to.
