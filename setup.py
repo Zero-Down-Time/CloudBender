962ef0571485e8a660a0e5e94dd773fc56f55aec
Stefan Reimer <stefan@zero-downtime.net>
2022-04-19 14:18:14 +0200

    fix: ensure Jinja2 version >=3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     },
     setup_requires=["setuptools-git-versioning"],
     description='Toolset to render and manage AWS Cloudformation',
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Stefan Reimer',
@@ -50,7 +50,7 @@ setup(
     package_data={ 'cloudbender': ['templates/*.md', 'templates/*.yaml'], },
     include_package_data=True,
     entry_points={'console_scripts': [ "cloudbender = cloudbender.cli:cli" ]},
-    install_requires=['boto3', 'Jinja2', 'click', 'cfn-lint>=0.34', 'pyminifier', 'pulumi>=3.0.0,<4.0.0', 'pulumi-aws>=4.0.0,<5.0.0'], #'apprise'
+    install_requires=['boto3', 'Jinja2>=3.0.0', 'click', 'cfn-lint>=0.34', 'pyminifier', 'pulumi>=3.0.0,<4.0.0', 'pulumi-aws>=4.0.0,<5.0.0'], #'apprise'
     tests_require=["pytest-cov", "moto", "mock", 'pytest'],
     cmdclass={"test": PyTest},
     classifiers=[
