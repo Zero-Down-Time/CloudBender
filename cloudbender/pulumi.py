00adf5a00f0f07688c89e683d869fba7f5961e8a
Stefan Reimer <stefan@zero-downtime.net>
2022-06-27 20:51:03 +0200

    feat: add execute task, rework Dockerfile to allow podman run rootless

--- cloudbender/pulumi.py
+++ cloudbender/pulumi.py
@@ -2,179 +2,179 @@ import sys
 import os
 import re
 import shutil
+import tempfile
 import importlib
-import pkg_resources
 import pulumi
+from functools import wraps
 
 import logging
 
 logger = logging.getLogger(__name__)
 
+# Fail early if pulumi binaries are not available
+if not shutil.which("pulumi"):
+    raise FileNotFoundError(
+        "Cannot find pulumi binary, see https://www.pulumi.com/docs/get-started/install/"
+    )
 
-def pulumi_init(stack, create=False):
 
-    # Fail early if pulumi binaries are not available
-    if not shutil.which("pulumi"):
-        raise FileNotFoundError(
-            "Cannot find pulumi binary, see https://www.pulumi.com/docs/get-started/install/"
-        )
+def pulumi_ws(func):
+    @wraps(func)
+    def decorated(self, *args, **kwargs):
+        # setup temp workspace
+        if self.mode == "pulumi":
+            self.work_dir = tempfile.mkdtemp(
+                dir=tempfile.gettempdir(), prefix="cloudbender-"
+            )
 
-    # add all artifact_paths/pulumi to the search path for easier imports in the pulumi code
-    for artifacts_path in stack.ctx["artifact_paths"]:
-        _path = "{}/pulumi".format(artifacts_path.resolve())
-        sys.path.append(_path)
+            # add all artifact_paths/pulumi to the search path for easier imports in the pulumi code
+            for artifacts_path in self.ctx["artifact_paths"]:
+                _path = "{}/pulumi".format(artifacts_path.resolve())
+                sys.path.append(_path)
 
-    # Try local implementation first, similar to Jinja2 mode
-    _found = False
-    try:
-        _stack = importlib.import_module(
-            "config.{}.{}".format(stack.rel_path, stack.template).replace("/", ".")
-        )
-        _found = True
-
-    except ImportError:
-        for artifacts_path in stack.ctx["artifact_paths"]:
+            # Try local implementation first, similar to Jinja2 mode
+            _found = False
             try:
-                spec = importlib.util.spec_from_file_location(
-                    "_stack",
-                    "{}/pulumi/{}.py".format(artifacts_path.resolve(), stack.template),
+                _stack = importlib.import_module(
+                    "config.{}.{}".format(self.rel_path, self.template).replace("/", ".")
                 )
-                _stack = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(_stack)
                 _found = True
 
-            except FileNotFoundError:
-                pass
+            except ImportError:
+                for artifacts_path in self.ctx["artifact_paths"]:
+                    try:
+                        spec = importlib.util.spec_from_file_location(
+                            "_stack",
+                            "{}/pulumi/{}.py".format(artifacts_path.resolve(), self.template),
+                        )
+                        _stack = importlib.util.module_from_spec(spec)
+                        spec.loader.exec_module(_stack)
+                        _found = True
 
-    if not _found:
-        raise FileNotFoundError(
-            "Cannot find Pulumi implementation for {}".format(stack.stackname)
-        )
+                    except FileNotFoundError:
+                        pass
 
-    # Store internal pulumi code reference
-    stack._pulumi_code = _stack
-    project_name = stack.parameters["Conglomerate"]
-
-    # Remove stacknameprefix if equals Conglomerate as Pulumi implicitly prefixes project_name
-    pulumi_stackname = re.sub(r"^" + project_name + "-?", "", stack.stackname)
-    try:
-        pulumi_backend = "{}/{}/{}".format(
-            stack.pulumi["backend"], project_name, stack.region
-        )
-
-    except KeyError:
-        raise KeyError("Missing pulumi.backend setting !")
-
-    account_id = stack.connection_manager.call(
-        "sts", "get_caller_identity", profile=stack.profile, region=stack.region
-    )["Account"]
-    # Ugly hack as Pulumi currently doesnt support MFA_TOKENs during role assumptions
-    # Do NOT set them via 'aws:secretKey' as they end up in the stack.json in plain text !!!
-    if (
-        stack.connection_manager._sessions[(stack.profile, stack.region)]
-        .get_credentials()
-        .token
-    ):
-        os.environ["AWS_SESSION_TOKEN"] = (
-            stack.connection_manager._sessions[(stack.profile, stack.region)]
-            .get_credentials()
-            .token
-        )
-
-    os.environ["AWS_ACCESS_KEY_ID"] = (
-        stack.connection_manager._sessions[(stack.profile, stack.region)]
-        .get_credentials()
-        .access_key
-    )
-    os.environ["AWS_SECRET_ACCESS_KEY"] = (
-        stack.connection_manager._sessions[(stack.profile, stack.region)]
-        .get_credentials()
-        .secret_key
-    )
-    os.environ["AWS_DEFAULT_REGION"] = stack.region
-
-    # Secrets provider
-    if "secretsProvider" in stack.pulumi:
-        secrets_provider = stack.pulumi["secretsProvider"]
-        if (
-            secrets_provider == "passphrase"
-            and "PULUMI_CONFIG_PASSPHRASE" not in os.environ
-        ):
-            raise ValueError("Missing PULUMI_CONFIG_PASSPHRASE environment variable!")
-
-    else:
-        try:
-            if stack._pulumi_code.IKNOWHATIDO:
-                logger.warning(
-                    "Missing pulumi.secretsProvider setting, IKNOWHATIDO enabled ... "
+            if not _found:
+                raise FileNotFoundError(
+                    "Cannot find Pulumi implementation for {}".format(self.stackname)
                 )
-                secrets_provider = None
-        except AttributeError:
-            raise ValueError("Missing pulumi.secretsProvider setting!")
 
-    # Set tag for stack file name and version
-    _tags = {}
-    try:
-        _version = stack._pulumi_code.VERSION
-    except AttributeError:
-        _version = "undefined"
+            # Store internal pulumi code reference
+            self._pulumi_code = _stack
 
-    # Tag all resources with our metadata, allowing "prune" eventually
-    _tags["zdt:cloudbender.source"] = "{}:{}".format(
-        os.path.basename(stack._pulumi_code.__file__), _version
-    )
-    _tags["zdt:cloudbender.owner"] = f"{project_name}.{pulumi_stackname}"
+            # Use legacy Conglomerate as Pulumi project_name
+            project_name = self.parameters["Conglomerate"]
 
-    _config = {
-        "aws:region": stack.region,
-        "aws:defaultTags": {"tags": _tags},
-        "zdt:region": stack.region,
-        "zdt:awsAccountId": account_id,
-        "zdt:projectName": project_name,
-        "zdt:stackName": pulumi_stackname
-    }
+            # Remove stacknameprefix if equals Conglomerate as Pulumi implicitly prefixes project_name
+            self.pulumi_stackname = re.sub(r"^" + project_name + "-?", "", self.stackname)
+            try:
+                pulumi_backend = "{}/{}/{}".format(
+                    self.pulumi["backend"], project_name, self.region
+                )
 
-    # inject all parameters as config in the <Conglomerate> namespace
-    for p in stack.parameters:
-        _config["{}:{}".format(stack.parameters["Conglomerate"], p)] = stack.parameters[
-            p
-        ]
+            except KeyError:
+                raise KeyError("Missing pulumi.backend setting !")
 
-    stack_settings = pulumi.automation.StackSettings(
-        config=_config,
-        secrets_provider=secrets_provider,
-        encryption_salt=stack.pulumi.get("encryptionsalt", None),
-        encrypted_key=stack.pulumi.get("encryptedkey", None),
-    )
+            account_id = self.connection_manager.call(
+                "sts", "get_caller_identity", profile=self.profile, region=self.region
+            )["Account"]
 
-    project_settings = pulumi.automation.ProjectSettings(
-        name=project_name, runtime="python", backend={"url": pulumi_backend}
-    )
+            # Ugly hack as Pulumi currently doesnt support MFA_TOKENs during role assumptions
+            # Do NOT set them via 'aws:secretKey' as they end up in the self.json in plain text !!!
+            if (
+                self.connection_manager._sessions[(self.profile, self.region)]
+                .get_credentials()
+                .token
+            ):
+                os.environ["AWS_SESSION_TOKEN"] = (
+                    self.connection_manager._sessions[(self.profile, self.region)]
+                    .get_credentials()
+                    .token
+                )
 
-    ws_opts = pulumi.automation.LocalWorkspaceOptions(
-        work_dir=stack.work_dir,
-        project_settings=project_settings,
-        stack_settings={pulumi_stackname: stack_settings},
-        secrets_provider=secrets_provider,
-    )
+            os.environ["AWS_ACCESS_KEY_ID"] = (
+                self.connection_manager._sessions[(self.profile, self.region)]
+                .get_credentials()
+                .access_key
+            )
+            os.environ["AWS_SECRET_ACCESS_KEY"] = (
+                self.connection_manager._sessions[(self.profile, self.region)]
+                .get_credentials()
+                .secret_key
+            )
+            os.environ["AWS_DEFAULT_REGION"] = self.region
 
-    if create:
-        pulumi_stack = pulumi.automation.create_or_select_stack(
-            stack_name=pulumi_stackname,
-            project_name=project_name,
-            program=stack._pulumi_code.pulumi_program,
-            opts=ws_opts,
-        )
-        pulumi_stack.workspace.install_plugin(
-            "aws", pkg_resources.get_distribution("pulumi_aws").version
-        )
+            # Secrets provider
+            if "secretsProvider" in self.pulumi:
+                secrets_provider = self.pulumi["secretsProvider"]
+                if (
+                    secrets_provider == "passphrase"
+                    and "PULUMI_CONFIG_PASSPHRASE" not in os.environ
+                ):
+                    raise ValueError("Missing PULUMI_CONFIG_PASSPHRASE environment variable!")
 
-    else:
-        pulumi_stack = pulumi.automation.select_stack(
-            stack_name=pulumi_stackname,
-            project_name=project_name,
-            program=stack._pulumi_code.pulumi_program,
-            opts=ws_opts,
-        )
+            else:
+                try:
+                    if self._pulumi_code.IKNOWHATIDO:
+                        logger.warning(
+                            "Missing pulumi.secretsProvider setting, IKNOWHATIDO enabled ... "
+                        )
+                        secrets_provider = None
+                except AttributeError:
+                    raise ValueError("Missing pulumi.secretsProvider setting!")
 
-    return pulumi_stack
+            # Set tag for stack file name and version
+            _tags = {}
+            try:
+                _version = self._pulumi_code.VERSION
+            except AttributeError:
+                _version = "undefined"
+
+            # Tag all resources with our metadata, allowing "prune" eventually
+            _tags["zdt:cloudbender.source"] = "{}:{}".format(
+                os.path.basename(self._pulumi_code.__file__), _version
+            )
+            _tags["zdt:cloudbender.owner"] = f"{project_name}.{self.pulumi_stackname}"
+
+            self.pulumi_config.update({
+                "aws:region": self.region,
+                "aws:defaultTags": {"tags": _tags},
+                "zdt:region": self.region,
+                "zdt:awsAccountId": account_id,
+                "zdt:projectName": project_name,
+                "zdt:stackName": self.pulumi_stackname
+            })
+
+            # inject all parameters as config in the <Conglomerate> namespace
+            for p in self.parameters:
+                self.pulumi_config["{}:{}".format(self.parameters["Conglomerate"], p)] = self.parameters[
+                    p
+                ]
+
+            stack_settings = pulumi.automation.StackSettings(
+                config=self.pulumi_config,
+                secrets_provider=secrets_provider,
+                encryption_salt=self.pulumi.get("encryptionsalt", None),
+                encrypted_key=self.pulumi.get("encryptedkey", None),
+            )
+
+            project_settings = pulumi.automation.ProjectSettings(
+                name=project_name, runtime="python", backend={"url": pulumi_backend}
+            )
+
+            self.pulumi_ws_opts = pulumi.automation.LocalWorkspaceOptions(
+                work_dir=self.work_dir,
+                project_settings=project_settings,
+                stack_settings={self.pulumi_stackname: stack_settings},
+                secrets_provider=secrets_provider,
+            )
+
+        response = func(self, *args, **kwargs)
+
+        # Cleanup temp workspace
+        if self.work_dir and os.path.exists(self.work_dir):
+            shutil.rmtree(self.work_dir)
+
+        return response
+
+    return decorated
