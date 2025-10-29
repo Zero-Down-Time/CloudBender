246b85c593734219e07a25e438584af626493e0d
Stefan Reimer <stefan@zero-downtime.net>
2025-10-29 14:38:50 +0000

    fix: use hatch version handling

--- cloudbender/__init__.py
+++ cloudbender/__init__.py
@@ -1,14 +1,9 @@
 import logging
-import importlib.metadata
+from ._version import __version_tuple__
 
 __author__ = "Stefan Reimer"
 __email__ = "stefan@zero-downtimet.net"
-
-try:
-    __version__ = importlib.metadata.distribution("CloudBender").version
-except importlib.metadata.PackageNotFoundError:
-    __version__ = "devel"
-
+__version__ = "{}.{}.{}-{}.{}".format(*__version_tuple__)
 
 # Set up logging to ``/dev/null`` like a library is supposed to.
 # http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
