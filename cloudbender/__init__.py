76723114d7d57616cda8e9bcf7c9c11c0733e7cf
Stefan Reimer <stefan@zero-downtime.net>
2025-11-26 10:48:57 +0000

    chore: update all minor deps

--- cloudbender/__init__.py
+++ cloudbender/__init__.py
@@ -7,6 +7,8 @@ __version__ = "{}.{}.{}-{}.{}".format(*__version_tuple__)
 
 # Set up logging to ``/dev/null`` like a library is supposed to.
 # http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
+
+
 class NullHandler(logging.Handler):  # pragma: no cover
     def emit(self, record):
         pass
