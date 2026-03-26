72ccb99395f78f95a2332c370fd53e821fa00f58
Stefan Reimer <stefan@zero-downtime.net>
2026-03-26 22:40:37 +0000

    feat: migrate dev workflow to uv

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,8 +8,7 @@ from cloudbender.utils import setup_logging
 def test_setup_logging_debug():
     logger = setup_logging(True)
     assert logger.getEffectiveLevel() == logging.DEBUG
-    assert logging.getLogger("botocore").getEffectiveLevel() == \
-        logging.INFO
+    assert logging.getLogger("botocore").getEffectiveLevel() == logging.INFO
 
     # Silence logging for the rest of the tests
     logger.setLevel(logging.CRITICAL)
