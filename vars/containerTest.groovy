95b179dbcbe6b1ed83339ceba980a09d7940ead6
Stefan Reimer <stefan@zero-downtime.net>
2026-04-29 12:55:02 +0000

    Squashed '.ci/' changes from ce8c46f..9f6936b
    9f6936b feat: make test phase optional
    
    git-subtree-dir: .ci
    git-subtree-split: 9f6936bf1234e002ac0bec17c0d4becfcde35275

--- vars/containerTest.groovy
+++ vars/containerTest.groovy
@@ -1,9 +1,13 @@
-// Test stage: placeholder for `just test` / `just container::test`
+// Test stage: optional `just test`, run if the recipe is defined
 def call(Map config = [:]) {
-    def workDir = config.workDir ?: '.'
+    def workDir     = config.workDir     ?: '.'
+    def needBuilder = config.needBuilder ?: false
 
     dir(workDir) {
-        sh "echo"
-        // sh "if just --summary | grep -q container::test; then just container::test; fi"
+        if (needBuilder) {
+            sh "if just --summary | grep -q test; then just use-builder test; fi"
+        } else {
+            sh "if just --summary | grep -q test; then just test; fi"
+        }
     }
 }
